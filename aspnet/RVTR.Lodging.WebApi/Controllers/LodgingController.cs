e7291b42a9cbfe4d19319483c4b5bd6e064ed91f
fred belotte <fredbelotte@outlook.com>
2020-04-21 23:08:43 -0500

    add web api project [#172281058]

---
+++ aspnet/RVTR.Lodging.WebApi/Controllers/LodgingController.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RVTR.Lodging.DataContext.Repositories;
+using RVTR.Lodging.ObjectModel.Models;
+
+namespace RVTR.Lodging.WebApi.Controllers
+{
+  [ApiController]
+  [EnableCors()]
+  [Route("[controller]")]
+  public class LodgingController : ControllerBase
+  {
+    private readonly ILogger<LodgingController> _logger;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public LodgingController(ILogger<LodgingController> logger, IUnitOfWork unitOfWork)
+    {
+      _logger = logger;
+      _unitOfWork = unitOfWork;
+    }
+
+    [HttpGet]
+    public async Task<LodgingModel> Get()
+    {
+      return await Task.FromResult<LodgingModel>(new LodgingModel());
+    }
+  }
+}
