84dda80eba708db7d8460549993cc48b7dc1fd5d
victor <vhdelcampo@miners.utep.edu>
2020-04-23 16:28:21 -0500

    add XML documentation for API ObjectModels [#172282259]

--- aspnet/RVTR.Lodging.ObjectModel/Models/BedModel.cs
+++ aspnet/RVTR.Lodging.ObjectModel/Models/BedModel.cs
@@ -7,17 +7,32 @@ namespace RVTR.Lodging.ObjectModel.Models
 {
     public class BedModel : IValidatableObject
     {
-
-
+        /// <summary>
+        /// Id of Bed.
+        /// </summary>
+        /// <value></value>
         [Required(ErrorMessage = "The BedId is required")]
         [Range(0, int.MaxValue, ErrorMessage = "The BedId number cannot be negative")]
         public int BedId { get; set; }
+        /// <summary>
+        /// Size of the Bed which can be King, Queen, Full, and Twin.
+        /// </summary>
+        /// <value></value>
         [Required(ErrorMessage = "The Size is required")]
         public string Size { get; set; }
         [Required(ErrorMessage = "The Amount is required")]
         [Range(0, int.MaxValue, ErrorMessage = "The Amount should be positive")]
+        /// <summary>
+        /// Number of Beds.
+        /// </summary>
+        /// <value></value>
         public int NumberOfBeds { get; set; }
 
+        /// <summary>
+        /// Validates that Size is one of four valid strings.
+        /// </summary>
+        /// <param name="validationContext"></param>
+        /// <returns></returns>
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
         {
 
--- aspnet/RVTR.Lodging.ObjectModel/Models/HotelModel.cs
+++ aspnet/RVTR.Lodging.ObjectModel/Models/HotelModel.cs
@@ -6,21 +6,62 @@ namespace RVTR.Lodging.ObjectModel.Models
 {
     public class HotelModel : IValidatableObject
     {
+        /// <summary>
+        /// Id of Hotel.
+        /// </summary>
+        /// <value></value>
         [Required(ErrorMessage = "The HotelId is required")]
         [Range(0, int.MaxValue, ErrorMessage = "The HotelId number cannot be negative")]
         public int HotelId { get; set; }
+        /// <summary>
+        /// Description of Hotel.
+        /// </summary>
+        /// <value></value>
         public string Description { get; set; }
+        /// <summary>
+        /// Type of Hotel.
+        /// </summary>
+        /// <value></value>
         public string Type { get; set; }
+        /// <summary>
+        /// Location of Hotel.
+        /// </summary>
+        /// <value></value>
         [Required(ErrorMessage = "The Location is required")]
         public LocationModel Location { get; set; }
+        /// <summary>
+        /// Area of Hotel. (Units determined by CultureInfo)
+        /// </summary>
+        /// <value></value>
         [Range(0, double.MaxValue, ErrorMessage = "The Area must be positive")]
         public double Area { get; set; }
+        /// <summary>
+        /// List of Reviwes of Hotel.
+        /// </summary>
+        /// <value></value>
         public List<ReviewModel> Reviews { get; set; }
+        /// <summary>
+        /// List of Rooms of Hotel.
+        /// </summary>
+        /// <value></value>
         [Required(ErrorMessage = "The rooms are required")]
         public List<RoomModel> Rooms { get; set; }
+        /// <summary>
+        /// List of Images of Hotel.
+        /// </summary>
+        /// <value></value>
         public List<ImageModel> Images { get; set; }
+        /// <summary>
+        /// List of Amenities of Hotel.
+        /// </summary>
+        /// <value></value>
         public List<AmenityModel> Amenities { get; set; }
 
+        /// <summary>
+        /// Validate that Rooms list contains at leats one Room.
+        /// </summary>
+        /// <param name="validationContext"></param>
+        /// <returns></returns>
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
         {
           if (Rooms.Count == 0)
